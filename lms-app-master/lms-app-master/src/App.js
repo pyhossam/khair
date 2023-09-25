2f101cdffc80bc1c32100608150c68ee6c56601b
pyhossam <hossam.kfuac@gmail.com>
2023-09-25 06:12:12 +0300

    update2-home

--- lms-app-master/lms-app-master/src/App.js
+++ lms-app-master/lms-app-master/src/App.js
@@ -1,41 +1,17 @@
 import logo from './logo.svg';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useAuth } from "./AuthContext";
-import Layout from './components/Layout';
-import Departments from './pages/Department/Departments';
-import Department from './pages/Program/Programs';
-import AddDepartment from './pages/Department/AddDepartment';
-import AddProgram from './pages/Program/AddProgram';
-import AddProject from './pages/progProjects/AddProject';
-import Header from './components/Header';
-import Auth from './pages/Auth';
+import RoutPage from './Route/Routes'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
-import Programs from './pages/Program/Programs';
-
 library.add(fas, faTwitter, faFontAwesome);
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <RoutPage />
+    
   );
 }
 
---
+++ lms-app-master/lms-app-master/src/Route/Routes.js
@@ -0,0 +1,31 @@
+// Routs.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuth } from "./AuthContext";
+import Layout from '../components/Layout/Layout';
+import Login from '../components/Home/Login';
+import RegistrationForm from '../components/Home/RegistrationForm';
+import Teacher from '../components/Teacher/Teacher';
+
+const RoutPage = () => {
+  const auth = useAuth();
+  const islogedin=(auth.user)?(true):(false);
+  return (
+
+    
+     
+      <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route path="/Login" element={<Login />} />
+          <Route index element={<Teacher islogged={islogedin} />} />
+          <Route path="/Teacher" element={<Teacher />} />
+          {/* <Route path="/Department/:id" element={<Department />} /> */}
+          
+        </Route>
+      </Routes>
+  
+  
+  );
+};
+
+export default RoutPage;
